ceeb0defaf131966e74542a9a70cbc13b0d172b9
наб <nabijaczleweli@gmail.com>
2020-06-06 17:55:05 +0200

    Use the hashState size instead of sum-of-members for the allocation
    The sizes matched on both amd64 and i686 but, well, let's not tempt the
    proverbial devil
    
    Ref: https://github.com/nabijaczleweli/md6-rs/issues/2

--- src/native.rs
+++ src/native.rs
@@ -1,10 +1,27 @@
-use libc::{c_void, malloc, free, c_int, c_uint, c_ulonglong};
+use libc::{c_void, malloc, free, c_int};
 use std::ptr::null_mut;
 use std::mem::size_of;
 
 
 pub type FFIHashState = *mut c_void;
 
+type BitSequence = u8;
+
+#[repr(C)]
+struct hashState {
+    hashbitlen: libc::c_int,
+    datalen: libc::c_int,
+    init: libc::c_int,
+    nullt: libc::c_int,
+    h32: [u32; 8],
+    t32: [u32; 2],
+    data32: [BitSequence; 64],
+    salt32: [u32; 4],
+    h64: [u64; 8],
+    t64: [u64; 2],
+    data64: [BitSequence; 128],
+    salt64: [u64; 4],
+}
 
 #[link(name = "blake")]
 extern "C" {
@@ -18,10 +35,7 @@ extern "C" {
 
 
 pub fn malloc_hash_state() -> FFIHashState {
-    unsafe {
-        malloc(size_of::<c_int>() * 4 + size_of::<c_uint>() * 8 + size_of::<c_uint>() * 2 + size_of::<u8>() * 64 + size_of::<c_uint>() * 4 +
-               size_of::<c_ulonglong>() * 8 + size_of::<c_ulonglong>() * 2 + size_of::<u8>() * 128 + size_of::<c_ulonglong>() * 4)
-    }
+    unsafe { malloc(size_of::<hashState>()) }
 }
 
 pub fn free_hash_state(state: &mut FFIHashState) {
